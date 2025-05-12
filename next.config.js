7da449e93ba08a2dcd7e138528552c1958626f4c
ETERNAL LABS <eternal@gardenx.xyz>
2025-05-11 23:08:17 -0700

    fix: add PostCSS and Tailwind configs, simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  poweredByHeader: false,
-  compress: true,
-  productionBrowserSourceMaps: false,
-  basePath: '',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig; 
