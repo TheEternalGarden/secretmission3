822beb66a167ff8a1d5a6b42a546d10861fed43b
ETERNAL LABS <eternal@gardenx.xyz>
2025-05-11 23:02:33 -0700

    chore: optimize Next.js build configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
+  productionBrowserSourceMaps: false,
+  basePath: '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
